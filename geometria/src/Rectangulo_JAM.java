4d33ec5649284be28163a99ce59ee544c9893ec1
JesúsAyala <jesusayalamilan2@gmail.com>
2022-03-01 01:26:21 +0100

    simepre positivo

--- geometria/src/Rectangulo_JAM.java
+++ geometria/src/Rectangulo_JAM.java
@@ -1,8 +1,18 @@
-
+/**
+ * 
+ * @author Jesús Ayala Milán
+ * @version 1.2
+ */
 public class Rectangulo_JAM extends FiguraGeometrica_JAM {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * método que da valor a los lados
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_JAM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
@@ -10,11 +20,22 @@ public class Rectangulo_JAM extends FiguraGeometrica_JAM {
 	}
 
 	@Override
+	/**
+	 * método que calcula el área
+	 * @param l1
+	 * @param l2
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
 	@Override
+	/**
+	 * método que calcula el perímetro
+	 * @param l1
+	 * @param l2
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
